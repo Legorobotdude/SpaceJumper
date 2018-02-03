32fa49bdc676299e3116139e577eb31aa09ac554
Aditya Bawankule <adityabawankule@gmail.com>
2018-02-03 03:17:49 -0600

    Created Scripts Folder

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+
+
+        }
+
+    }
+}

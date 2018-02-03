fb45d89a0b4384f88dcfcbeddfd4f6c2127bd3aa
Aditya Bawankule <adityabawankule@gmail.com>
2018-02-03 03:35:49 -0600

    Audio Playing (2)

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -23,6 +25,16 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+           
+
+        }
+        else
+        {
+            audioSource.Stop();
 
         }
         if (Input.GetKey(KeyCode.A))

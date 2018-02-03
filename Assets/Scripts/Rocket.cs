1d5978fbe319d83c28596dc36f373fe32d3a66bc
Aditya Bawankule <adityabawankule@gmail.com>
2018-02-03 03:30:26 -0600

    Added Sound Files

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -27,14 +27,14 @@ public class Rocket : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.A))
         {
-
+            transform.Rotate(Vector3.forward);
 
 
         }
         else if (Input.GetKey(KeyCode.D))
         {
 
-
+            transform.Rotate(-Vector3.forward);
 
         }
 
